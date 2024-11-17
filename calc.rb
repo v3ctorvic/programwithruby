e71fa913fa25b39b8cb50cf4a401d458af7a9b7d
v3ctorvic <vmg1977@gmail.com>
2024-11-16 22:22:22 -0700

    Add calc.rb and practice.rb

---
+++ calc.rb
@@ -0,0 +1,20 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
+
+puts "Hours in a year: #{24 * 365}"
+
+puts "Minutes in a decade: #{(2 * 24 * 60) + (365 * 24 * 60 * 10)}"
+
+puts "I am #{60 * 60 * 24 * 365 * 45} seconds old."
+
+puts "The author is #{1390000000 / (60 * 60 * 24 * 365)}"
---
+++ practice.rb
@@ -0,0 +1,21 @@
+# puts 12 + 12
+# puts "12" + "12"
+# puts "12 + 12"
+
+# puts 2 * 5
+# puts "2" * 5
+# puts "2 * 5"
+
+# puts "12" + 12
+# puts "2" * "5"
+
+# puts "They said, \"Yes!\""
+# puts "up\\down"
+# puts "backlash at the end of a string: \\"
+# puts "Dip!"
+# puts "\D\i\p\!"
+
+puts "Duck...\nDuck..."
+puts "\tGoose! (or grey duck for some of you)"
+
+puts
