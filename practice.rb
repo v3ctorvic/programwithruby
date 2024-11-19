68d955ad3c5e3a6cf4b3a0be40a3d33179b6d8bd
v3ctorvic <vmg1977@gmail.com>
2024-11-19 12:22:47 -0700

    Added updates from work on chapter 6

--- practice.rb
+++ practice.rb
@@ -209,3 +209,59 @@
 # puts(Math.tan(Math::PI/4))
 # puts(Math.log(Math::E**2))
 # puts((1 + Math.sqrt(5))/2) #golden ratio
+
+#Chapter 6 - Flow Control, examples below:
+# puts 1 > 2
+# puts 1 < 2
+# puts ""
+# puts 5 >= 5
+# puts 5 <= 4
+# puts ""
+# puts 1 == 1
+# puts 2 != 1
+# puts ""
+# puts "cat" < "dog"
+# puts ""
+# puts "a giant woman"          < "Watermelon Steven"
+# puts "a giant woman".downcase < "Watermelon Steven".downcase
+# puts ""
+# puts 2 < 10
+# puts "2" < "10"
+
+# branching examples:
+# puts "Hello, what's your name?"
+# name = gets.chomp
+# puts "Hello, #{name}."
+
+# if name == "Chris"
+#   puts "what a lovely name!"
+# end
+
+#fortune-teller branching example
+# puts "I am a fortune-teler. Tell me your name:"
+# name = gets.chomp
+
+# if name == "Victor"
+#   puts "I see many great things in your future."
+# else
+#   puts "Your future is...oh my! Look at the time!"
+#   puts "I really have to go. Sorry!"
+# end
+
+puts "Hello, and welcome to seventh grade English."
+puts "My name is Mrs. Gabbard. And your name is...?"
+name = gets.chomp
+
+if name == name.capitalize
+  puts "Please take a seat, #{name}."
+else
+  puts "#{name}? You mean #{name.capitalize}, right?"
+  puts "Don't you even know how to spell your name??"
+  reply = gets.chomp
+
+  if reply.downcase == "yes"
+    puts "Hmmph! Well, sit down!"
+  else
+    puts "GET OUT!!"
+  end
+end
